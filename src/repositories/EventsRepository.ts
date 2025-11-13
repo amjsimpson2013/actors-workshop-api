d953ed1abc0d3f6f0e4a4766c5fa8e96f45213e2
Kal <amjsimpson2013@gmail.com>
2025-11-12 20:49:20 -0500

    36-added endpoint for getting events by types

--- src/repositories/EventsRepository.ts
+++ src/repositories/EventsRepository.ts
@@ -1,4 +1,4 @@
-import { EventSummaryDto, mapSummaryFromDb } from "../models/EventDto";
+import { EventDetailDto, EventSummaryDto, mapDetailsFromDb, mapSummaryFromDb } from "../models/EventDto";
 import { db } from "../utils/db/ActorsWorkshopContext";
 import { Events } from "../utils/db/context";
 
@@ -16,5 +16,26 @@ export class EventsRepository {
         const mappedResults: EventSummaryDto[] = [];
         results.forEach((res) => mappedResults.push(mapSummaryFromDb(res)));
         return mappedResults;
+    }
+
+    public async getEventsByType(eventTypeId: number): Promise<EventSummaryDto[]> {
+        const results = await this.database.selectFrom('events')
+            .select(['id', 'name', 'description', 'start_date', 'end_date', 'thumbnail_name', 'thumbnail_type'])
+            .where('event_type_id', '=', eventTypeId)
+            .execute() as unknown as Events[];
+
+        const mappedResults: EventSummaryDto[] = [];
+        results.forEach((res) => mappedResults.push(mapSummaryFromDb(res)));
+        return mappedResults;
+    }
+
+    public async getEventById(eventId: number): Promise<EventDetailDto> {
+        const result = await this.database.selectFrom('events')
+            .selectAll('events')
+            .where('id', '=', eventId)
+            .executeTakeFirst() as unknown as Events;
+        
+        const mappedResult: EventDetailDto = mapDetailsFromDb(result);
+        return mappedResult;
     }
 }
